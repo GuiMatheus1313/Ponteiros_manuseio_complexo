afd7f693bcfd38f9637ed7a0ef11287d4145a225
Guilherme Araujo <82453465+GuiMatheus1313@users.noreply.github.com>
2024-03-10 17:00:02 +0000

    Passo a passo da alocação de ponteiros

--- .ccls-cache/@home@runner@Ponteirosmanuseiocomplexo/main.c
+++ .ccls-cache/@home@runner@Ponteirosmanuseiocomplexo/main.c
@@ -10,7 +10,24 @@ int main(void) {
   printf("A: %d e B: %d\n", a, b);
   p1 = &a;
   printf("p1 = %p e &p1 = %p\n", p1, &p1);
-  
+  p2 = p1;
   printf("p1 = %p e &p1 = %p\n", p1, &p1);
+  printf("p2 = %p e &p2 = %p\n\n", p2, &p2);
+
+  printf("Resultado: %d\n", *p2 = *p1 + 3);
+  printf("p1 = %d e &p1 = %p\n", *p1, &p1);
+  printf("p2 = %d e &p2 = %p\n", *p2, &p2);
+  printf("A: %d e B: %d\n\n\n", a, b);
+
+  b = b*(*p1);
+  printf("A: %d e B: %d\n", a, b);
+  printf("*p2++ = %d \n\n\n\n", a); //PERGUNTAR PARA O PROFESSOR PRÓXIMA AULA sobre o ++ e não mudar o número
+
+  p1 = &b;
+  printf("%d %d\n", *p1, *p2);
+  printf("%d %d\n", a, b);
+
+  
+
 
 }
